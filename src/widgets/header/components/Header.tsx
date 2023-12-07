24f01a1155c6b8a9878bee46b4458ee146fab6d3
igor.aparin <igor.aparin@red-soft.ru>
2023-12-07 15:55:24 +0300

    feat: add header component

---
+++ src/widgets/header/components/Header.tsx
@@ -0,0 +1,8 @@
+export const Header = () => {
+  return (
+    <header className="header">
+      <h1 className="header__title">{'Board game, mission creation helper app'}</h1>
+      <div className="header__logo">{'Battle Herald'}</div>
+    </header>
+  );
+};
