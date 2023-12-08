5246384c513f381f5be464ddff348edcdc2b27be
igor.aparin <igor.aparin@red-soft.ru>
2023-12-08 15:35:53 +0300

    feat: add card component

---
+++ src/shared/ui/card/InfoCard.tsx
@@ -0,0 +1,29 @@
+export type TCardParagraph = {
+  paragraphTitle: string;
+  paragraphBody: string;
+};
+
+export interface InfoCardProps {
+  cardType: string;
+  cardTitle: string;
+  cardContent: TCardParagraph[];
+}
+
+export const InfoCard = ({ cardType, cardTitle, cardContent }: InfoCardProps) => {
+  return (
+    <div className="info-card">
+      <div className="info-card__head">
+        <span className="info-card__type">{cardType}</span>
+        <h4 className="info-card__title">{cardTitle}</h4>
+      </div>
+      <div className="info-card__body">
+        {cardContent.map((elem) => (
+          <div className="info-card-paragraph">
+            <h5 className="info-card-paragraph__title">{elem.paragraphTitle}</h5>
+            <p className="info-card-paragraph__body">{elem.paragraphBody}</p>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
