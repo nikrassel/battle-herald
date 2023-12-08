1561eac74df83af5ade8b6a329b1a17704feac41
igor.aparin <igor.aparin@red-soft.ru>
2023-12-08 17:47:53 +0300

    feat: add card-row

--- src/pages/leviathanGame/LeviathanGame.tsx
+++ src/pages/leviathanGame/LeviathanGame.tsx
@@ -1,7 +1,11 @@
+import { CardRow } from '@//entities/cardRow/CardRow';
+import { primaryMissions } from './lib/constants/primaryMissions';
+
 export const LeviathanGame = () => {
   return (
     <main className="page">
       <h3>Leviathan Game settings</h3>
+      <CardRow cards={primaryMissions} />
       {/* <InfoCard {...someCard} /> */}
     </main>
   );
---
+++ src/pages/leviathanGame/lib/constants/primaryMissions.ts
@@ -0,0 +1,44 @@
+import { InfoCardProps } from '@//shared/ui';
+
+export const primaryMissions: InfoCardProps[] = [
+  {
+    cardType: 'card type',
+    cardTitle: 'card title',
+    cardContent: [
+      {
+        paragraphTitle: 'text',
+        paragraphBody: 'text',
+      },
+    ],
+  },
+  {
+    cardType: 'card type',
+    cardTitle: 'card title',
+    cardContent: [
+      {
+        paragraphTitle: 'text',
+        paragraphBody: 'text',
+      },
+    ],
+  },
+  {
+    cardType: 'card type',
+    cardTitle: 'card title',
+    cardContent: [
+      {
+        paragraphTitle: 'text',
+        paragraphBody: 'text',
+      },
+    ],
+  },
+  {
+    cardType: 'card type',
+    cardTitle: 'card title',
+    cardContent: [
+      {
+        paragraphTitle: 'text',
+        paragraphBody: 'text',
+      },
+    ],
+  },
+];
