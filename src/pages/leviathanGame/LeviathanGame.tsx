17c840e84d00cb4d19fdea2e6dbb0a82e2718a19
igor.aparin <igor.aparin@red-soft.ru>
2023-12-11 10:24:49 +0300

    feat: improve card style and typezation

--- src/pages/leviathanGame/LeviathanGame.tsx
+++ src/pages/leviathanGame/LeviathanGame.tsx
@@ -5,7 +5,9 @@ export const LeviathanGame = () => {
   return (
     <main className="page">
       <h3>Leviathan Game settings</h3>
-      <CardRow cards={primaryMissions} />
+      <div className="page__cards-layout">
+        <CardRow cards={primaryMissions} />
+      </div>
       {/* <InfoCard {...someCard} /> */}
     </main>
   );
--- src/pages/leviathanGame/lib/constants/primaryMissions.ts
+++ src/pages/leviathanGame/lib/constants/primaryMissions.ts
@@ -2,12 +2,31 @@ import { InfoCardProps } from '@//shared/ui';
 
 export const primaryMissions: InfoCardProps[] = [
   {
-    cardType: 'card type',
-    cardTitle: 'card title',
+    cardType: 'primary mission',
+    cardTitle: 'take and hold',
+    cardContent: [
+      {
+        paragraphTitle: 'Second, third and fourth battle rounds',
+        paragraphBody: [
+          'At the end of each Command phase, the player whose turn it is sxores 5VP for each objective marker they control (up tp 15VP per turn).',
+        ],
+      },
+      {
+        paragraphTitle: 'In the fifth battle round',
+        paragraphBody: [
+          'The player who has the first turn scores VP as described above.',
+          'The player who has the second turn scores VP as described above, but does so at the end of their turn instead of at the end of their Command phase.',
+        ],
+      },
+    ],
+  },
+  {
+    cardType: 'primary mission',
+    cardTitle: 'take and hold',
     cardContent: [
       {
         paragraphTitle: 'text',
-        paragraphBody: 'text',
+        paragraphBody: ['text'],
       },
     ],
   },
@@ -17,7 +36,7 @@ export const primaryMissions: InfoCardProps[] = [
     cardContent: [
       {
         paragraphTitle: 'text',
-        paragraphBody: 'text',
+        paragraphBody: ['text'],
       },
     ],
   },
@@ -27,17 +46,7 @@ export const primaryMissions: InfoCardProps[] = [
     cardContent: [
       {
         paragraphTitle: 'text',
-        paragraphBody: 'text',
-      },
-    ],
-  },
-  {
-    cardType: 'card type',
-    cardTitle: 'card title',
-    cardContent: [
-      {
-        paragraphTitle: 'text',
-        paragraphBody: 'text',
+        paragraphBody: ['text'],
       },
     ],
   },
