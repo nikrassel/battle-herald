e618cce0bfe5eebaca51f895ca665d3174a3a492
igor.aparin <igor.aparin@red-soft.ru>
2023-12-19 09:19:55 +0300

    feat: add control panel for resume game page

--- src/pages/currentGame/components/index.ts
+++ src/pages/currentGame/components/index.ts
@@ -1,1 +1,2 @@
 export * from './CurrentLeviathanGame';
+export * from './CurrentGameControlPanel';
--- src/pages/currentGame/connectors/CurrentGameConnector.tsx
+++ src/pages/currentGame/connectors/CurrentGameConnector.tsx
@@ -1,20 +1,25 @@
 import { useContext } from 'react';
+import { useNavigate } from 'react-router-dom';
 
 import { GameContext } from '@//app/context';
 import { localStorageService } from '@//app/localStorage';
-import { CurrentLeviathanGame } from '..';
+
+import { CurrentGameControlPanel, CurrentLeviathanGame } from '..';
 
 export const CurrentGameConnector = () => {
   const context = useContext(GameContext);
+  const navigate = useNavigate();
   const currentGame = localStorageService.checkCurrentGame();
+  if (!context?.isCurrentGame || !currentGame) {
+    navigate('/');
+  }
   return (
     <main className="page">
       <h3>Current Game settings</h3>
-      {context?.isCurrentGame && currentGame ? (
-        <CurrentLeviathanGame missionId={currentGame} />
-      ) : (
-        <h2>Вы еще не начали игру</h2>
-      )}
+      {currentGame && <CurrentLeviathanGame missionId={currentGame} />}
+      <div className="page__control-panel">
+        <CurrentGameControlPanel />
+      </div>
     </main>
   );
 };
