24f3e49d5b9c072f9517af292dabef604c98747d
nikrassel <nikrassel@mail.ru>
2023-12-29 10:24:15 +0300

    feat: add deployments images

--- src/shared/tempDB/constants/deployments.ts
+++ src/shared/tempDB/constants/deployments.ts
@@ -5,26 +5,31 @@ export const deployments: TInfoCardDeck = {
   [DeploymentsNames.hammerAndAnvil]: {
     cardType: 'deployment',
     cardTitle: 'hammer and anvil',
+    cardImage: '/HammerAndAnvil.png',
     cardContent: [],
   },
   [DeploymentsNames.sweepingEngagement]: {
     cardType: 'deployment',
     cardTitle: 'sweeping engagement',
+    cardImage: '/SweepingEngagement.png',
     cardContent: [],
   },
   [DeploymentsNames.dawnOfWar]: {
     cardType: 'deployment',
     cardTitle: 'dawn of war',
+    cardImage: '/DawnOfWar.png',
     cardContent: [],
   },
   [DeploymentsNames.searchAndDestroy]: {
     cardType: 'deployment',
     cardTitle: 'search and destroy',
+    cardImage: '/SearchAndDestroy.png',
     cardContent: [],
   },
   [DeploymentsNames.crucibleOfBattle]: {
     cardType: 'deployment',
     cardTitle: 'crucible of battle',
+    cardImage: '/CrucibleOfBattle.png',
     cardContent: [],
   },
 };
